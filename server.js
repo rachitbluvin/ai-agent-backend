8c88c3dc2c0f3c0ac33e25d7afcd0610f11dbfae
rachit012 <rachitthakur012@gmail.com>
2026-01-07 14:05:02 +0530

    added public port

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(errorHandler);
 
 const start = async () => {
   await connectDB();
-  app.listen(PORT, () => {
+  app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server is running on port ${PORT}`);
   });
 };
